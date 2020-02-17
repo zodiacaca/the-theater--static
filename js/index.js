f6e9bedfc252469fb3177a824958b25b6f33591c
zodiacaca <121059464@qq.com>
2020-02-17 16:43:10 +0800

    u

--- js/index.js
+++ js/index.js
@@ -8,3 +8,11 @@ $("#main").click(function(eventData) {
     $(".slider").removeClass("slider--open")
   }
 })
+
+const url = "http://192.168.10.11:4000/videos/output/stream.mpd"
+const player = dashjs.MediaPlayer().create()
+player.initialize($("video")[0], url, false)
+player.on("canPlay", function() {
+  // const bitrates = player.getBitrateInfoListFor("video")
+  // player.setQualityFor("video", 0)
+})
