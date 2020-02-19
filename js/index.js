647bcbaf0407b3abe5dcfea71381171df13bd8f4
zodiacaca <121059464@qq.com>
2020-02-19 16:32:25 +0800

    u

--- js/index.js
+++ js/index.js
@@ -18,15 +18,18 @@ const covers = {
 $(".list-item").click(function(eventData) {
   $(".slider").addClass("slider--open")
 })
+$(".slider").click(function(eventData) {
+  $(".slider").addClass("slider--open")
+})
 
 $("#main").click(function(eventData) {
-  if (eventData.target.className.includes("board-ctn")) {
+  if (!eventData.target.className.includes("list")) {
     $(".slider").removeClass("slider--open")
   }
 })
 
 // dash
-const url = "http://192.168.10.11:4000/videos/output/stream.mpd"
+const url = "http://192.168.10.11:3000/videos/output/stream.mpd"
 const player = dashjs.MediaPlayer().create()
 player.initialize($("video")[0], url, false)
 player.on("canPlay", function() {
